7f81624546c1e909c6b28f2aa553edff0e9c635e
James Howard <james@ivcaccounting.co.uk>
2025-12-05 11:04:59 +0000

    Switch to simpler non-standalone deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   eslint: {
     // Disable ESLint during build - we'll run it separately
     ignoreDuringBuilds: true,
-  },
-  // Required for standalone mode
-  experimental: {
-    outputFileTracingRoot: undefined,
   },
 }
 
